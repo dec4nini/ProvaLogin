67517d7f1aff3bf4310c52f5e4cca28ea8e38b5a
EduardoDecanini <eduardodecanini@gmail.com>
2021-06-21 12:49:47 -0300

    projetoFinalizado

---
+++ ProvaLogin/ProvaLogin/Data/IESContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using ProvaLogin.Models.Infra;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProvaLogin.Data
+{
+    public class IESContext : IdentityDbContext<UsuarioDaAplicacao>
+    {
+        public IESContext(DbContextOptions<IESContext> options) : base(options)
+        {
+
+        }
+    }
+}
---
+++ ProvaLogin/ProvaLogin/Data/IESDbInitializer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProvaLogin.Data
+{
+    public class IESDbInitializer
+    {
+        public static void Initialize(IESContext context)
+        {
+            
+            context.Database.EnsureCreated();
+
+            context.SaveChanges();
+        }
+    }
+}
